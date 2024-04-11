d867432603bc6e015c4c6ab69b5474b4b0fb4b7b
baiching <bachmarma@gmail.com>
2024-04-11 22:27:11 +0200

    added edit, delete and complete feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
+import React, { useState } from 'react';
+
 type Todo = {
   id: number;
   text: string;
   done: boolean;
 };
 
-import React, { useState } from "react";
-
-function App(){
-  const[todos, setTodos] = useState<Todo[]>([]);
+function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
   const [inputValue, setInputValue] = useState('');
 
   const addTodo = () => {
@@ -20,22 +20,56 @@ function App(){
     setInputValue('');
   };
 
+  const toggleDone = (id: number) => {
+    const newTodos = todos.map(todo => {
+      if (todo.id === id) {
+        return { ...todo, done: !todo.done };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (id: number) => {
+    const newTodos = todos.filter(todo => todo.id !== id);
+    setTodos(newTodos);
+  };
+
+  const updateTodo = (id: number, newText: string) => {
+    const newTodos = todos.map(todo => {
+      if (todo.id === id) {
+        return { ...todo, text: newText };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
       <h1>Todo List</h1>
       <input
-      value={inputValue}
-      onChange={(e) => setInputValue(e.target.value)}
-      placeholder="Add a new todo"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Add a new todo"
       />
-      <button onClick={addTodo}> Add Todo</button>
+      <button onClick={addTodo}>Add Todo</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.text}</li>
+          <li key={todo.id} style={{ textDecoration: todo.done ? 'line-through' : 'none' }}>
+            <input
+              type="text"
+              value={todo.text}
+              onChange={(e) => updateTodo(todo.id, e.target.value)}
+              disabled={todo.done}
+            />
+            <button onClick={() => toggleDone(todo.id)}>{todo.done ? 'Undo' : 'Complete'}</button>
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
         ))}
       </ul>
     </div>
   );
 }
 
-export default App
+export default App;
